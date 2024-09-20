496d17f3b391a3a7fc33c8f2ad6cb83de992cf05
Еремишин Никита Николаевич <n.eremishin@stc-spb.ru>
2024-09-20 20:24:13 +0300

    Handle POST user request

--- user.py
+++ user.py
@@ -7,8 +7,17 @@ def get_user():
     user_id = request.args.get("id")
     # todo Получение пользователя по id из БД
     # todo Вернуть данные о пользователе
-    return user_id or "Icnorrect user id"
+    return user_id or "Incorrect user id"
+
+@user_blueprint.route('/', methods=['DELETE'])
+def delete_user():
+    pass
 
 @user_blueprint.route('/', methods=['POST'])
 def register_user():
-    pass
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        json = request.json
+        # todo Запись данных в БД
+        return "Success"
+    return 'Content-Type not supported!'
